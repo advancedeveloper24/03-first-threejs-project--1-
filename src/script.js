79c94fe622ecc5dc11afe0a3b9f6fc3a3203f9c8
Kevin Everrett <advancedeveloper24@gmail.com>
2025-01-05 17:45:59 -0500

    Moving Camera

--- src/script.js
+++ src/script.js
@@ -1,5 +1,33 @@
 import * as THREE from "three";
 
-//Scene
+//Canvas
+const canvas = document.querySelector("canvas.webgl");
 
+//Scene
 const scene = new THREE.Scene();
+
+//Geometry
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: "#6CA6CD" });
+//add to mesh
+const mesh = new THREE.Mesh(geometry, material);
+//add mesh to scene
+scene.add(mesh);
+// Sizes
+const sizes = {
+  width: 800,
+  height: 600,
+};
+
+//Camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+camera.position.z = 3;
+scene.add(camera);
+
+//Renderer
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+});
+renderer.setSize(sizes.width, sizes.height);
+
+renderer.render(scene, camera);
