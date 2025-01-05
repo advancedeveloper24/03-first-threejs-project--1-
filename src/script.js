d0e73cda0ff0fcfd9af5800f8dfe7ebc37b26d92
Kevin Everrett <advancedeveloper24@gmail.com>
2025-01-05 18:45:49 -0500

    Transforming Objects

--- src/script.js
+++ src/script.js
@@ -6,23 +6,68 @@ const canvas = document.querySelector("canvas.webgl");
 //Scene
 const scene = new THREE.Scene();
 
+//Objects
+const group = new THREE.Group();
+
+const cube1 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: "#6CA6CD" })
+);
+
+const cube2 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: "#ad0a11" })
+);
+
+const cube3 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: "#7fc126" })
+);
+cube1.position.set(1, 1, 1);
+cube3.position.set(-1, -1, -1);
+scene.add(cube1, cube2, cube3);
 //Geometry
 const geometry = new THREE.BoxGeometry(1, 1, 1);
 const material = new THREE.MeshBasicMaterial({ color: "#6CA6CD" });
 //add to mesh
 const mesh = new THREE.Mesh(geometry, material);
+//Position
+mesh.position.y = 1.5;
+mesh.position.x = 1;
+mesh.position.z = -10;
+
+//console.log(mesh.position.length());
+//console.log(mesh.position.distanceTo(camera.position));
+//mesh.position.normalize();
+mesh.position.set(1.3, 1, -5);
+
+//Scale
+mesh.scale.x = 2;
+mesh.scale.y = 6;
+mesh.scale.z = 6;
+
+//Rotation
+mesh.rotation.y = -8;
+mesh.rotation.x = 4;
+mesh.rotation.z = 3;
+
+mesh.rotation.set(Math.PI * 0.25, Math.PI * 0.5, Math.PI * 0.25);
+
 //add mesh to scene
-scene.add(mesh);
+//scene.add(mesh);
+//Camera
 // Sizes
 const sizes = {
   width: 800,
   height: 600,
 };
-
-//Camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
 camera.position.z = 3;
 scene.add(camera);
+
+//Axes Helper - helps with determining axes of object
+const axesHelper = new THREE.AxesHelper(3);
+//scene.add(axesHelper);
 
 //Renderer
 const renderer = new THREE.WebGLRenderer({
