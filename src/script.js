bbd85670f5c90979ec866911df7515b3e34ee88d
Kevin Everrett <advancedeveloper24@gmail.com>
2025-01-05 19:29:20 -0500

    elapsedTime

--- src/script.js
+++ src/script.js
@@ -7,25 +7,25 @@ const canvas = document.querySelector("canvas.webgl");
 const scene = new THREE.Scene();
 
 //Objects
-const group = new THREE.Group();
+//const group = new THREE.Group();
 
-const cube1 = new THREE.Mesh(
-  new THREE.BoxGeometry(1, 1, 1),
-  new THREE.MeshBasicMaterial({ color: "#6CA6CD" })
-);
+//const cube1 = new THREE.Mesh(
+//new THREE.BoxGeometry(1, 1, 1),
+//new THREE.MeshBasicMaterial({ color: "#6CA6CD" })
+//);
 
-const cube2 = new THREE.Mesh(
-  new THREE.BoxGeometry(1, 1, 1),
-  new THREE.MeshBasicMaterial({ color: "#ad0a11" })
-);
+//const cube2 = new THREE.Mesh(
+// new THREE.BoxGeometry(1, 1, 1),
+// new THREE.MeshBasicMaterial({ color: "#ad0a11" })
+//);
 
-const cube3 = new THREE.Mesh(
-  new THREE.BoxGeometry(1, 1, 1),
-  new THREE.MeshBasicMaterial({ color: "#7fc126" })
-);
-cube1.position.set(1, 1, 1);
-cube3.position.set(-1, -1, -1);
-scene.add(cube1, cube2, cube3);
+//const cube3 = new THREE.Mesh(
+// new THREE.BoxGeometry(1, 1, 1),
+// new THREE.MeshBasicMaterial({ color: "#7fc126" })
+//);
+//cube1.position.set(1, 1, 1);
+//cube3.position.set(-1, -1, -1);
+//scene.add(cube1, cube2, cube3);
 //Geometry
 const geometry = new THREE.BoxGeometry(1, 1, 1);
 const material = new THREE.MeshBasicMaterial({ color: "#6CA6CD" });
@@ -39,22 +39,23 @@ mesh.position.z = -10;
 //console.log(mesh.position.length());
 //console.log(mesh.position.distanceTo(camera.position));
 //mesh.position.normalize();
-mesh.position.set(1.3, 1, -5);
+mesh.position.set(0, 0, 0);
 
 //Scale
-mesh.scale.x = 2;
-mesh.scale.y = 6;
-mesh.scale.z = 6;
+//mesh.scale.x = 2;
+//mesh.scale.y = 6;
+//mesh.scale.z = 6;
 
 //Rotation
-mesh.rotation.y = -8;
-mesh.rotation.x = 4;
-mesh.rotation.z = 3;
+//mesh.rotation.y = -8;
+/////mesh.rotation.x = 4;
+//mesh.rotation.z = 3;
 
-mesh.rotation.set(Math.PI * 0.25, Math.PI * 0.5, Math.PI * 0.25);
+//mesh.rotation.set(Math.PI * 0.25, Math.PI * 0.5, Math.PI * 0.25);
 
 //add mesh to scene
-//scene.add(mesh);
+scene.add(mesh);
+
 //Camera
 // Sizes
 const sizes = {
@@ -66,7 +67,7 @@ camera.position.z = 3;
 scene.add(camera);
 
 //Axes Helper - helps with determining axes of object
-const axesHelper = new THREE.AxesHelper(3);
+//const axesHelper = new THREE.AxesHelper(3);
 //scene.add(axesHelper);
 
 //Renderer
@@ -76,3 +77,29 @@ const renderer = new THREE.WebGLRenderer({
 renderer.setSize(sizes.width, sizes.height);
 
 renderer.render(scene, camera);
+
+//Animations
+let time = Date.now();
+
+const clock = new THREE.Clock();
+
+const tick = () => {
+  // clock
+  const elapsedTime = clock.getElapsedTime();
+  console.log(elapsedTime);
+  // time
+  const currentTime = Date.now();
+  const deltaTime = currentTime - time;
+
+  //update objects
+  //mesh.rotation.x = elapsedTime * Math.PI * 2;
+  //mesh.position.y = Math.sin(elapsedTime);
+  camera.position.x = Math.cos(elapsedTime);
+  camera.position.y = Math.sin(elapsedTime);
+  camera.lookAt(mesh.position);
+  //render
+  renderer.render(scene, camera);
+
+  window.requestAnimationFrame(tick);
+};
+tick();
